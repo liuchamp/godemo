0c4263113259cafe2a44da24bed24a3c830a48c9
yexingyun <2756154878@qq.com>
2019-06-08 14:24:07 +0700

    feat(init): redis简单使用

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
-import "github.com/go-redis/redis"
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
 
 func main() {
 	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{":7000", ":7001", ":7002", ":7003", ":7004", ":7005"},
+		Addrs: []string{
+			"192.168.0.193:7001",
+			"192.168.0.193:7002",
+			"192.168.0.193:7003",
+			"192.168.0.193:7004",
+			"192.168.0.193:7005",
+			"192.168.0.193:7006"},
 	})
 	redisdb.Ping()
+	err := redisdb.ReloadState()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
