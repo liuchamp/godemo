8386e36e50f37873784c70d266a641c0b6d03066
yexingyun <2756154878@qq.com>
2019-06-10 10:39:51 +0700

    feat(redis): 单客户端连接成功，能正常写入值
    注意端口， 这里只有7005可用， 其他端口的不能使用

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	yrd "github.com/garyburd/redigo/redis"
 	"github.com/go-redis/redis"
 )
 
 func main() {
-	ExampleNewClient()
-	ExampleClient()
+	ExampleNewClient2()
+}
+func ExampleClientGary() {
+	c, err := yrd.Dial("tcp", "192.168.0.193:7003")
+	if err != nil {
+		fmt.Println("Connect to redis error", err)
+		return
+	}
+	defer c.Close()
+
+	_, err = c.Do("SET", "mykey", "superWang")
+	if err != nil {
+		fmt.Println("redis set failed:", err)
+	}
+
+	username, err := yrd.String(c.Do("GET", "mykey"))
+	if err != nil {
+		fmt.Println("redis get failed:", err)
+	} else {
+		fmt.Printf("Get mykey: %v \n", username)
+	}
 }
 func ExampleNewClient() {
 	client := redis.NewClient(&redis.Options{
-		Addr: "192.168.0.193:7001",
+		Addr: "192.168.0.193:7005",
 		// Password: "", // no password set
 		DB: 0, // use default DB
 	})
@@ -19,11 +39,45 @@ func ExampleNewClient() {
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 	// Output: PONG <nil>
+
+	Get := func(client *redis.Client, key string) *redis.StringCmd {
+		cmd := redis.NewStringCmd("get", key)
+		client.Process(cmd)
+		return cmd
+	}
+
+	v, err := Get(client, "key_does_not_exist").Result()
+	fmt.Printf("%q %s \n", v, err)
+
+	v, e := client.Do("get", "key_does_not_exist").String()
+	fmt.Printf("%q %s \n", v, e)
+
+}
+func ExampleNewClient2() {
+	client := redis.NewClient(&redis.Options{
+		Addr: "192.168.0.193:7005",
+		// Password: "", // no password set
+		DB: 0, // use default DB
+	})
+	defer client.Close()
+
+	SAVE := func(client *redis.Client, key string, value string) *redis.StringCmd {
+		cmd := redis.NewStringCmd("set", key, value)
+		client.Process(cmd)
+		return cmd
+	}
+
+	v, err := SAVE(client, "key_does_not_exist", "smul").Result()
+	fmt.Printf("%q %s \n", v, err)
+
+	v, e := client.Do("get", "key_does_not_exist").String()
+	fmt.Printf("%q %s \n", v, e)
+
 }
 
 func ExampleClient() {
 	client := redis.NewClient(&redis.Options{
-		Addr: "92.168.0.193:7001",
+		Addr: "92.168.0.193:7003",
 		// Password: "", // no password set
 		DB: 0, // use default DB
 	})
