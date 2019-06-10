32c1a987e1e2bf774ad9f07d8d819801560ba03e
yexingyun <2756154878@qq.com>
2019-06-10 19:10:52 +0700

    feat(mgo): 通过password连接有问题

--- main.go
+++ main.go
@@ -2,22 +2,76 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-redis/redis"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"strings"
+	"time"
 )
 
+type Person struct {
+	Name  string
+	Phone string
+}
+
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
+	test1()
+}
+func test1() {
+	session, err := mgo.Dial("mongodb://192.168.0.193:27017,mongodb://192.168.0.193:27018,mongodb://192.168.0.193:27019")
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
+}
+
+func test2() {
+	host := "mongodb://192.168.0.193:27017,mongodb://192.168.0.193:27018,mongodb://192.168.0.193:27019"
+	hosts := strings.Split(host, ",")
+	mongoDBDialInfo := mgo.DialInfo{
+		Addrs:    hosts,
+		Timeout:  60 * time.Second,
+		Database: "yexingyun",
+		Username: "",
+		Password: "",
+	}
+	var err error
+	mongoSession, err := mgo.DialWithInfo(&mongoDBDialInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer mongoSession.Close()
+	mongoSession.SetMode(mgo.Monotonic, true)
+	c := mongoSession.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
 }
