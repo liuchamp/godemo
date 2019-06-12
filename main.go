a1b7fea4e5ba5a2ac752ab3d5ba8d3779257833b
yexingyun <yexingyun9210@gmail.com>
2019-06-12 21:13:06 +0700

    feat(chanel): 通道的使用
    消息机制认为每个并发单元是自包含的、独立的个体，并且都有自己的变量，但在不同并发单元间这些变量不共享。每个并发单元的输入和输出只有一种，那就是消息。
    
    　　channel
    是 Go 语言在语言级别提供的 goroutine 间的通信方式，我们可以使用 channel 在多个 goroutine
    之间传递消息。channel是进程内的通信方式，因此通过 channel
    传递对象的过程和调用函数时的参数传递行为比较一致，比如也可以传递指针等。channel 是类型相关的，一个 channel
    只能传递一种类型的值，这个类型需要在声明 channel 时指定。
    
    　　channel的声明形式为：
    
    1
    var chanName chan
    ElementType
    　　举个例子，声明一个传递int类型的channel：
    
    var ch chan int
     　　使用内置函数
    make() 定义一个channel：
    
    ch := make(chan
    int)
    　　在channel的用法中，最常见的包括写入和读出：
    
    //
    将一个数据value写入至channel，这会导致阻塞，直到有其他goroutine从这个channel中读取数据
    ch <-
    value
    
    // 从channel中读取数据，如果channel之前没有写入数据，也会导致阻塞，直到channel中被写入数据为止
    value
    := <-ch
    默认情况下，channel的接收和发送都是阻塞的，除非另一端已准备好。
    
     我们还可以创建一个带缓冲的channel：
    
    c
    := make(chan int, 1024)
    
    // 从带缓冲的channel中读数据
    for i:=range c
    {
    　　...
    }
    此时，创建一个大小为1024的int类型的channel，即使没有读取方，写入方也可以一直往channel里写入，在缓冲区被填完之前都不会阻塞。
    
    可以关闭不再使用的channel：
    
    close(ch)
    应该在生产者的地方关闭channel，如果在消费者的地方关闭，容易引起panic；
    
    现在利用channel来重写上面的例子：
    
    例子中，定义了一个包含10个channel的数组，并把数组中的每个channel分配给10个不同的goroutine。在每个goroutine完成后，向goroutine写入一个数据，在这个channel被读取前，这个操作是阻塞的。在所有的goroutine启动完成后，依次从10个channel中读取数据，在对应的channel写入数据前，这个操作也是阻塞的。这样，就用channel实现了类似锁的功能，并保证了所有goroutine完成后main()才返回。
    
    　　另外，我们在将一个channel变量传递到一个函数时，可以通过将其指定为单向channel变量，从而限制该函数中可以对此channel的操作。

--- main.go
+++ main.go
@@ -2,35 +2,26 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
 )
 
-var couter int = 0
-
-func Count(lock *sync.Mutex) {
-	lock.Lock()
-	couter++
-	fmt.Println("counter = ", couter)
-	lock.Unlock()
+func Count(cha chan int) {
+	cha <- 1
+	fmt.Println("Counting")
 }
 
 func main() {
-	lock := &sync.Mutex{}
 
+	chs := make([]chan int, 10)
 	for i := 0; i < 10; i++ {
-		go Count(lock)
-
+		chs[i] = make(chan int)
+		go Count(chs[i])
 	}
 
-	for {
-		lock.Lock()
-		c := couter
-		lock.Unlock()
-		runtime.Gosched()
-		if c >= 10 {
-			break
-		}
+	var cout int
+	for _, ch := range chs {
+
+		cout += <-ch
+		fmt.Println(cout)
 	}
 }
 
