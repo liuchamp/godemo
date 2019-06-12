dda28f30717aae7ab24f48cb3675c71278d04d27
yexingyun <yexingyun9210@gmail.com>
2019-06-12 21:46:53 +0700

    feat(chanel): 没有理解清楚select的原理
    例如下面的代码中， 关于通过的使用过程中没有对应的select的使用问题

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-
+	timeout := make(chan bool, 1)
 	ch := make(chan int)
-	for i := 0; i < 10; i++ {
-		select {
-		case ch <- i:
-		case x := <-ch:
-			fmt.Println(x)
-		}
+	go func() {
+		time.Sleep(1e9) // sleep one second
+		ch <- 1
+		timeout <- true
+	}()
+	select {
+	case u := <-ch:
+		fmt.Println(u)
+	case <-timeout:
+		fmt.Println("timeout!")
 	}
 }
