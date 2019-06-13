9862bf39c3bd8ac9a45783abcd71f5e278c7ff5b
yexingyun <yexingyun9210@gmail.com>
2019-06-13 20:16:25 +0700

    feat(pointer): 指针和变量的关系
    取地址操作符&和取值操作符*是一对互补操作符，&取出地址，*根据地址取出地址指向的值。
    
    变量、指针地址、指针变量、取地址、取值的相互关系和特性如下：
    对变量进行取地址（&）操作，可以获得这个变量的指针变量。
    指针变量的值是指针地址。
    对指针变量进行取值（*）操作，可以获得指针变量指向的原变量的值。

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	getPr()
+	Changes()
 }
 
 func getPr() {
@@ -23,4 +23,23 @@ func getPr() {
 	// 指针取值后就是指向变量的值
 	fmt.Printf("value: %s\n", value)
 
+}
+
+func Changes() {
+	// 准备两个变量, 赋值1和2
+	x, y := 1, 2
+	// 交换变量值
+	swap(&x, &y)
+	// 输出变量值
+	fmt.Println(x, y)
+}
+
+// 交换函数
+func swap(a, b *int) {
+	// 取a指针的值, 赋给临时变量t
+	t := *a
+	// 取b指针的值, 赋给a指针指向的变量
+	*a = *b
+	// 将a指针的值赋给b指针指向的变量
+	*b = t
 }
