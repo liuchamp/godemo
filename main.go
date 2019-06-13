54f9ee7b261a06fe78dfae3e4e6208ff1525547b
yexingyun <yexingyun9210@gmail.com>
2019-06-13 21:27:48 +0700

    feat(Closure): 闭包
    闭包对它作用域上部变量的引用可以进行修改，修改引用的变量就会对变量进行实际修改，通过下面的例子来理解

--- main.go
+++ main.go
@@ -92,3 +92,16 @@ func an2op() {
 		fmt.Println("skill not found")
 	}
 }
+
+func clousuretest() {
+	// 准备一个字符串
+	str := "hello world"
+	// 创建一个匿名函数
+	foo := func() {
+
+		// 匿名函数中访问str
+		str = "hello dude"
+	}
+	// 调用匿名函数
+	foo()
+}
