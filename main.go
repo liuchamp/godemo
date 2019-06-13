6a8b61eb94319e46d38cfa18aa2713b1a83e1cf2
yexingyun <yexingyun9210@gmail.com>
2019-06-13 21:06:48 +0700

    feat(chain): 链式处理

--- main.go
+++ main.go
@@ -2,22 +2,54 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-redis/redis"
+	"strings"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
-	if err != nil {
-		fmt.Println(err)
+	chanstest1()
+}
+
+func chanstest1() {
+	// 待处理的字符串列表
+	list := []string{
+		"go scanner",
+		"go parser",
+		"go compiler",
+		"go printer",
+		"go formater",
 	}
+	// 处理函数链
+	chain := []func(string) string{
+		removePrefix,
+		strings.TrimSpace,
+		strings.ToUpper,
+	}
+	// 处理字符串
+	StringProccess(list, chain)
+	// 输出处理好的字符串
+	for _, str := range list {
+		fmt.Println(str)
+	}
+
+}
+
+// 字符串处理函数，传入字符串切片和处理链
+func StringProccess(list []string, chain []func(string) string) {
+	// 遍历每一个字符串
+	for index, str := range list {
+		// 第一个需要处理的字符串
+		result := str
+		// 遍历每一个处理链
+		for _, proc := range chain {
+			// 输入一个字符串进行处理，返回数据作为下一个处理链的输入。
+			result = proc(result)
+		}
+		// 将结果放回切片
+		list[index] = result
+	}
+}
+
+// 自定义的移除前缀的处理函数
+func removePrefix(str string) string {
+	return strings.TrimPrefix(str, "go")
 }
