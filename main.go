2eaa2501d5c23837ea3de86557ce07ddd314ed22
yexingyun <yexingyun9210@gmail.com>
2019-06-13 20:11:18 +0700

    feat(pointer): 指针的使用详解
    指针， 生成和使用

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"github.com/go-redis/redis"
-)
+import "fmt"
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
-	if err != nil {
-		fmt.Println(err)
-	}
+
+	getPr()
+}
+
+func getPr() {
+	// 准备一个字符串类型
+	var house = "Malibu Point 10880, 90265"
+	// 对字符串取地址, ptr类型为*string
+	ptr := &house
+	// 打印ptr的类型
+	fmt.Printf("ptr type: %T\n", ptr)
+	// 打印ptr的指针地址
+	fmt.Printf("address: %p\n", ptr)
+	// 对指针进行取值操作
+	value := *ptr
+	// 取值后的类型
+	fmt.Printf("value type: %T\n", value)
+	// 指针取值后就是指向变量的值
+	fmt.Printf("value: %s\n", value)
+
 }
