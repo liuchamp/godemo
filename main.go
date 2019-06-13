bde9d46a0f12d4a388cf8c71dce0fb2c6bbb668f
yexingyun <yexingyun9210@gmail.com>
2019-06-13 21:35:13 +0700

    feat(Closure): 闭包实现生成器
    闭包的记忆效应进程被用于实现类似于设计模式中工厂模式的生成器。下面的例子展示了创建一个玩家生成器的过程。

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	clousuretest2()
+	gentest()
 }
 
 func chanstest1() {
@@ -133,4 +133,23 @@ func Accumulate(value int) func() int {
 		// 返回一个累加值
 		return value
 	}
+}
+
+// 创建一个玩家生成器, 输入名称, 输出生成器
+func playerGen(name string) func() (string, int) {
+	// 血量一直为150
+	hp := 150
+	// 返回创建的闭包
+	return func() (string, int) {
+		// 将变量引用到闭包中
+		return name, hp
+	}
+}
+func gentest() {
+	// 创建一个玩家生成器
+	generator := playerGen("high noon")
+	// 返回玩家的名字和血量
+	name, hp := generator()
+	// 打印值
+	fmt.Println(name, hp)
 }
