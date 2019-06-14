e882560d1acc0e40947ddce61d0c0b90bc364b7a
liuchamp <liucheng1010299461@163.com>
2019-06-14 14:24:42 +0700

    feat(goroutine): 定时器使用问题
    定时器本身也就是一个通道

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	ti()
+}
+
+func ti1() {
 	timeout := make(chan bool, 1)
 	ch := make(chan int)
 	go func() {
@@ -19,4 +23,15 @@ func main() {
 	case <-timeout:
 		fmt.Println("timeout!")
 	}
+}
+func ti() {
+	//初始化定时器
+	t := time.NewTimer(2 * time.Second)
+	//当前时间
+	now := time.Now()
+	fmt.Printf("Now time : %v.\n", now)
+	fmt.Printf("n type : %T.\n", 2)
+
+	expire := <-t.C
+	fmt.Printf("Expiration time: %v.\n", expire)
 }
