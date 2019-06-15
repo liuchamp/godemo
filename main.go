e9e1ef5b3aa0cf09c78436a6fd421a43f1906523
liuchamp <yexingyun9210@gmail.com>
2019-06-15 17:26:17 +0700

    feat(yaml): 使用yaml解析文件

--- main.go
+++ main.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/go-redis/redis"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
+//定义conf类型
+//类型里的属性，全是配置文件里的属性
+type conf struct {
+	Host   string `yaml: "host"`
+	User   string `yaml:"user"`
+	Pwd    string `yaml:"pwd"`
+	Dbname string `yaml:"dbname"`
+}
+
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
+	var c conf
+	//读取yaml配置文件
+	conf := c.getConf()
+	fmt.Println(conf)
+
+	//将对象，转换成json格式
+	data, err := json.Marshal(conf)
+
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("err:\t", err.Error())
+		return
 	}
+
+	//最终以json格式，输出
+	fmt.Println("data:\t", string(data))
+}
+
+//读取Yaml配置文件,
+//并转换成conf对象
+func (c *conf) getConf() *conf {
+	//应该是 绝对地址
+	yamlFile, err := ioutil.ReadFile("E:\\Program\\go2\\goPath\\src\\xingej-go\\xingej-go\\xingej-go666\\lib\\yaml\\conf.yaml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	err = yaml.Unmarshal(yamlFile, c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return c
 }
