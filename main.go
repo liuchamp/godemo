873e4bcdf1176e24cc157e1ccb853db14c43335c
yexingyun <yexingyun9210@gmail.com>
2019-07-27 16:22:20 +0700

    feat(viper): 使用viper解析文件和环境变量

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 func main() {
+	demoEvn()
+}
 
+func demo1() {
 	vYml := viper.New()
 	vYml.SetConfigName("app")  // name of config file (without extension)
 	vYml.AddConfigPath("conf") // path to look for the config file in
@@ -16,4 +22,46 @@ func main() {
 	fmt.Println(vYml.ReadInConfig())
 	urls := vYml.GetString("redis.urls")
 	fmt.Println(urls)
+}
+
+func demo2() {
+	vYml := viper.New()
+	vYml.SetConfigName("application")
+	vYml.AddConfigPath("conf")
+	if err := vYml.ReadInConfig(); err != nil {
+		log.Fatal("配置读写失败", err)
+	}
+	var deappconfig defaultAppConfig
+	if err := vYml.UnmarshalKey("app", &deappconfig); err != nil {
+		log.Fatal("配置解析失败", err)
+	}
+	v, _ := json.Marshal(deappconfig)
+	fmt.Print(string(v))
+}
+
+type defaultAppConfig struct {
+	Port int64
+	Name string
+	Tags []string
+}
+
+func demoEvn() {
+
+	os.Setenv("SPF_PORT", "13")
+	os.Setenv("SPF_NAME", "13")
+	os.Setenv("SPF_TAGS", "13,dsaigsdaf")
+
+	vEnv := viper.New()
+	vEnv.SetEnvPrefix("spf")
+	vEnv.AutomaticEnv()
+	vEnv.BindEnv("port")
+	vEnv.BindEnv("name")
+	vEnv.BindEnv("tags")
+
+	var deappconfig defaultAppConfig
+	if err := vEnv.Unmarshal(&deappconfig); err != nil {
+		log.Fatal("配置解析失败", err)
+	}
+	v, _ := json.Marshal(deappconfig)
+	fmt.Print(string(v))
 }
