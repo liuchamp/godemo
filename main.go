6a052e1468d90f664d77120f45c7daab9f34755a
yexingyun <yexingyun9210@gmail.com>
2019-07-29 18:00:44 +0700

    feat(ip): 获取IP地址

--- main.go
+++ main.go
@@ -1,23 +1,78 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-	"github.com/go-redis/redis"
+	"io"
+	"net"
+	"net/http"
+	"os"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
+	demo2()
+}
+
+func demo1() {
+	addrs, err := net.InterfaceAddrs()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
+	bs, err := json.Marshal(addrs)
+	if err == nil {
+		fmt.Println(string(bs))
+	}
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Println(ipnet.IP.String())
+			}
+		}
+	}
+}
+
+var get_ip = flag.String("get_ip", "", "external|internal")
+
+func demo2() {
+	fmt.Println("Usage of ./getmyip --get_ip=(external|internal)")
+	flag.Parse()
+	if *get_ip == "external" {
+		get_external()
+	}
+
+	if *get_ip == "internal" {
+		get_internal()
+	}
+
+}
+
+func get_external() {
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Stderr.WriteString("\n")
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+	os.Exit(0)
+}
+
+func get_internal() {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops:" + err.Error())
+		os.Exit(1)
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+			}
+		}
+	}
+	os.Exit(0)
 }
