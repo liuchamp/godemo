d9c6b0627ba80b99786a64834f52cbf0c09ae71f
yexingyun <yexingyun9210@gmail.com>
2019-07-29 21:38:31 +0700

    feat(gin logger): 简单的logger

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-redis/redis"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
 	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
-	if err != nil {
-		fmt.Println(err)
-	}
+
+	router.Run()
 }
