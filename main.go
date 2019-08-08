436d622dc5700604d5ba661814567d88166722ba
yexingyun <yexingyun9210@gmail.com>
2019-08-08 17:59:00 +0700

    feat(json): json转map

--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/go-redis/redis"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
-	if err != nil {
-		fmt.Println(err)
-	}
+	j := `{
+    "Name": "王老五",
+    "Oks": "dsadsfa"
+}`
+	var jm map[string]interface{}
+	json.Unmarshal([]byte(j), &jm)
+	fmt.Print(jm)
 }
