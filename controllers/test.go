2f4c756b5d81bfa2daaa36a1ed101788ef13f8bd
yexingyun <yexingyun9210@gmail.com>
2019-08-10 11:18:43 +0700

    feat(gin): 读取gin请求内容
    读取request.body，转化为string

--- controllers/test.go
+++ controllers/test.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/liuchamp/godemo/services"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -15,7 +18,15 @@ func (ts *TestController) Demo(c *gin.Context) {
 	c.JSON(http.StatusOK, t)
 }
 func (ts *TestController) Menu(c *gin.Context) {
-
+	getUser(c)
 	t := ts.Service.GetOne()
 	c.JSON(http.StatusOK, t)
+}
+func getUser(c *gin.Context) map[string]interface{} {
+	var btm map[string]interface{}
+
+	s, _ := ioutil.ReadAll(c.Request.Body)
+	json.Unmarshal(s, &btm)
+	fmt.Println(btm)
+	return btm
 }
