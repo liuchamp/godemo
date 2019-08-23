5aa4f74e7b10abd9d3329802a4fed790b8775c46
yexingyun <yexingyun9210@gmail.com>
2019-08-23 11:39:00 +0700

    feat(prt): 指针值传递

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Masterminds/goutils"
 )
 
 func main() {
-	s := make(map[string]int64)
-	ddms(s)
-	prting(s)
-	ddms(s)
-	prting(s)
-	ddms(s)
-	prting(s)
+	Muts()
+}
+
+type NameStdt struct {
+	D string
+	E string
+}
+
+// 验证对象clone
+func Muts() {
+	sd := new(NameStdt)
+	sd.D = "dsafdsa"
+	sd.E = "dsafdsi"
+
+	od(sd)
+	sdbs, _ := json.Marshal(sd)
+	fmt.Println(string(sdbs))
+	od(sd)
+	sdbs, _ = json.Marshal(sd)
+	fmt.Println(string(sdbs))
+}
+
+func od(s *NameStdt) {
+	smk, _ := goutils.RandomAlphabetic(9)
+	s.E = smk
+	smd, _ := goutils.RandomAlphabetic(9)
+	s.D = smd
 }
 
 func ddms(ks map[string]int64) error {
