a12d3bef786f3cf1222eb2d47a81cedea27d1b61
yexingyun <yexingyun9210@gmail.com>
2019-09-02 20:06:47 +0700

    feat(mongddb ): connect

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-redis/redis"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://192.168.0.193:27017"))
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+	err = client.Connect(context.TODO())
+	col := client.Database("casbin").Collection("casbin_rule")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cor, err := col.Find(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+	if cor.Next(ctx) {
+
 	}
 }
