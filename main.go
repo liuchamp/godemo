727f2d8c957bd5b788cf186af54782dfd87f8bca
yexingyun <yexingyun9210@gmail.com>
2019-09-06 20:15:48 +0700

    feat(time的使用):

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
+	"windplatform/webbackend/webserver/util"
 )
+
+type DemoDec struct {
+	UpdateTime time.Time
+	CreateTime time.Time
+}
+
+const timeform = "2006-01-02 15:04:05"
 
 func main() {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://192.168.0.193:27017"))
@@ -14,13 +23,35 @@ func main() {
 		panic(err)
 	}
 	err = client.Connect(context.TODO())
-	col := client.Database("casbin").Collection("casbin_rule")
+	col := client.Database("test").Collection("tezt")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	cor, err := col.Find(ctx, bson.M{})
+	ddc := DemoDec{}
+	ddc.CreateTime = time.Now()
+	ddc.UpdateTime = time.Now()
+
+	_, err = col.InsertOne(ctx, ddc)
 	if err != nil {
 		panic(err)
 	}
-	if cor.Next(ctx) {
 
+	filter := bson.M{"createtime": bson.M{"$lt": time.Now().Add(-2 * time.Hour), "$gt": time.Now().Add(-4 * time.Hour)}}
+	opt := options.FindOptions{}
+	var lim int64 = 101
+	var sikp int64 = 0
+	opt.Limit = &lim
+	opt.Skip = &sikp
+	cor, err := col.Find(ctx, filter, &opt)
+	if err != nil {
+		panic(err)
 	}
+	defer cor.Close(ctx)
+
+	for cor.Next(ctx) {
+		sm := new(DemoDec)
+		if err := cor.Decode(sm); err != nil {
+			panic(err)
+		}
+		fmt.Println(util.TimeFormat(sm.CreateTime), "------>", util.TimeFormat(sm.UpdateTime))
+	}
+
 }
