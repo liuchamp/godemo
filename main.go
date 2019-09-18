652495067548b0c7b6e23f606f517779d4274d09
yexingyun <yexingyun9210@gmail.com>
2019-09-18 22:45:12 +0700

    feat(interface): 内嵌接口的使用

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"github.com/go-redis/redis"
-)
+type Actor interface {
+	OnConnect() error
+	OnMessage(data []byte) error
+	OnError(err error)
+	OnClose()
+	GetMsgChan() <-chan []byte
+	Done() <-chan struct{}
+}
+type TCPCtrl struct {
+	Actor
+}
+
+func (t *TCPCtrl) Scan() {
+	defer t.OnClose()
+	err := t.OnConnect()
+	if err != nil {
+		t.OnError(err)
+	}
+Circle:
+	for {
+		select {
+		case <-t.Done():
+			break Circle
+		case msg := <-t.GetMsgChan():
+			err := t.OnMessage(msg)
+			if err != nil {
+				t.OnError(err)
+			}
+		}
+	}
+}
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
-	if err != nil {
-		fmt.Println(err)
-	}
+
 }
