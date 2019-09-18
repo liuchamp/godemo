27554ed334a8235471fac6cc948ea0f1db92a3cb
yexingyun <yexingyun9210@gmail.com>
2019-09-18 22:54:03 +0700

    feat(label): break和continue在label作用下，控制代码执行流程

--- main.go
+++ main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-redis/redis"
+	"time"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
-	if err != nil {
-		fmt.Println(err)
+	bdemo()
+}
+
+func bdemo() {
+	FirstNames := []string{"aaa", "bbb", "ccc"}
+	LastNames := []string{"111", "222", "333"}
+
+Loop:
+	for _, firstName := range FirstNames {
+		for _, lastName := range LastNames {
+			fmt.Printf("Name: %s %s\n", firstName, lastName)
+			time.Sleep(time.Second)
+			if firstName == "bbb" && lastName == "111" {
+				break Loop
+			}
+		}
 	}
+	println("Over.")
+}
+
+func demos() {
+	FirstNames := []string{"aaa", "bbb", "ccc"}
+	LastNames := []string{"111", "222", "333"}
+
+Loop:
+	for _, firstName := range FirstNames {
+		for _, lastName := range LastNames {
+			fmt.Printf("Name: %s %s %d\n", firstName, lastName, time.Now().Unix())
+			time.Sleep(time.Second)
+			if firstName == "bbb" && lastName == "111" {
+				continue Loop
+			}
+		}
+	}
+	println("Over.")
 }
