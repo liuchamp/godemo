319b0601b5d81d2c8ec04569b523f98c87491aeb
yexingyun <yexingyun9210@gmail.com>
2019-11-12 22:54:42 +0700

    feat(index): 使用mongo创建index

--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-redis/redis"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://192.168.0.193:27017"))
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+	err = client.Connect(context.TODO())
+
+	col := client.Database("test").Collection("sdmo")
+	indexs := col.Indexes()
+
+	ctx := context.TODO()
+	i := mongo.IndexModel{
+		Keys: bson.M{"sd": 1},
+	}
+
+	// 创建唯一索引，需要保证记录中，有对应的字段，否则不能创建，重复创建时，不会报错
+	i.Options = options.Index().SetUnique(true)
+	s, er := indexs.CreateOne(ctx, i, options.CreateIndexes())
+	if er != nil {
+		log.Fatal(err)
+	} else {
+		log.Println(s)
+	}
+	cor, err := indexs.List(ctx)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer cor.Close(ctx)
+
+	for cor.Next(ctx) {
+		im := new(mongo.IndexModel)
+		if e := cor.Decode(im); e != nil {
+			log.Println(e)
+			return
+		} else {
+			log.Println(im)
+		}
+
 	}
 }
