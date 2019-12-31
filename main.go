0a690baf04fa4e78cd4ece11e6b7fcbd70fd2a96
yexingyun <yexingyun9210@gmail.com>
2019-12-31 19:14:33 +0700

    简单实用cache

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+type User struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	usec := cache.New(5*time.Minute, 6*time.Minute)
 	usec.Set("user1111", 1000, cache.DefaultExpiration)
@@ -16,5 +21,16 @@ func main() {
 	s2, ok := usec.Get("user1112")
 	if ok {
 		println(s2.(string))
+	}
+	usec.Set("users", User{
+		Name: "yyyyyy",
+		Age:  56,
+	}, cache.DefaultExpiration)
+
+	s3, ok := usec.Get("users")
+	if ok {
+		uinfo := s3.(User)
+		println(uinfo.Name)
+		println("ok")
 	}
 }
