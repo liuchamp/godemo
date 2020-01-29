23c5104e1a368685ba0af47bf39ad1077e75fac4
yexingyun <yexingyun9210@gmail.com>
2020-01-29 16:56:33 +0700

    简单tg bot 的使用

--- main.go
+++ main.go
@@ -1,23 +1,74 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-redis/redis"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("1"),
+		tgbotapi.NewKeyboardButton("2"),
+		tgbotapi.NewKeyboardButton("3"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("4"),
+		tgbotapi.NewKeyboardButton("5"),
+		tgbotapi.NewKeyboardButton("6"),
+	),
 )
 
 func main() {
-	redisdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			"192.168.0.193:7001",
-			"192.168.0.193:7002",
-			"192.168.0.193:7003",
-			"192.168.0.193:7004",
-			"192.168.0.193:7005",
-			"192.168.0.193:7006"},
-	})
-	redisdb.Ping()
-	err := redisdb.ReloadState()
+	bot, err := tgbotapi.NewBotAPI("821401577:AAEJ5D6O8evdFul6bd4pc1hBuSQsI8jfzbA")
 	if err != nil {
-		fmt.Println(err)
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	rmsg := tgbotapi.NewMessage(-375956450, "bot is ok")
+	if _, err := bot.Send(rmsg); err != nil {
+		log.Panic(err)
+	}
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, _ := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if !update.Message.IsCommand() { // ignore any non-command Messages
+			continue
+		}
+
+		// Create a new MessageConfig. We don't have text yet,
+		// so we should leave it empty.
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		println("to------>", update.Message.Chat.ID)
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "type /sayhi or /status."
+		case "sayhi":
+			msg.Text = "Hi :)"
+		case "status":
+			msg.Text = "I'm ok."
+		case "open":
+			msg.ReplyMarkup = numericKeyboard
+			msg.Text = update.Message.Text
+		case "close":
+			msg.Text = update.Message.Text
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
 	}
 }
