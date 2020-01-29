749aa02ba69b15f697886a23554ba00c18d8d820
yexingyun <yexingyun9210@gmail.com>
2020-01-29 21:02:21 +0700

    执行命令不成功

--- main.go
+++ main.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/mem"
 	"os"
-	"os/exec"
-	"strconv"
 )
 
 func main() {
 
-	pid := os.Getegid()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/info", func(c *gin.Context) {
+		pid := os.Getpid()
+		v, _ := mem.VirtualMemory()
+		//cm,_:=cpu.Info()
+		fmt.Printf("进程 PID: %d \n", pid)
 
-	fmt.Printf("进程 PID: %d \n", pid)
-
-	prc := exec.Command("ps", "-p", strconv.Itoa(pid), "-v")
-	out, err := prc.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(out))
+		//prc := exec.Command("bash", "-c", "top -n 1 -p "+string(pid))
+		//out, err := prc.CombinedOutput()
+		//if err != nil {
+		//	panic(err)
+		//}
+		//fmt.Println(string(out))
+		c.JSON(200, gin.H{
+			"Total":       v.Total,
+			"Free":        v.Free,
+			"UsedPercent": v.UsedPercent,
+		})
+	})
+	r.Run(":18880")
 }
