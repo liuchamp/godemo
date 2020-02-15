d3ee0223f9c553a3c195bd314ced453e1426089f
yexingyun <yexingyun9210@gmail.com>
2020-02-15 16:37:33 +0700

    测试顺序问题

--- wire.go
+++ wire.go
@@ -5,6 +5,6 @@ package main
 import "github.com/google/wire"
 
 func InitializeEvent() Event {
-	wire.Build(NewEvent, NewGreeter, NewMessage)
+	wire.Build(NewGreeter, NewEvent, NewMessage)
 	return Event{}
 }
