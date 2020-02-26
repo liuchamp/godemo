fe6c38d3e77b85f2c847a84cd85403bd84d54803
yexingyun <yexingyun9210@gmail.com>
2020-02-26 11:01:16 +0700

    feat(Closure): 子命令中的pre等函数和父命令的是覆盖关系

--- cmd/test.go
+++ cmd/test.go
@@ -33,7 +33,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		fmt.Println("test PersistentPreRun", args)
+		fmt.Printf("name is : %s \n", name)
 	},
 
 	Run: func(cmd *cobra.Command, args []string) {
