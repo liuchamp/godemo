2e57dff49960b93aa6ba4577c9094e16409fbb3a
yexingyun <yexingyun9210@gmail.com>
2020-02-27 15:33:55 +0700

    test json

--- main.go
+++ main.go
@@ -15,8 +15,37 @@ limitations under the License.
 */
 package main
 
-import "github.com/liuchamp/godemo/cmd"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type PlayerRealFlow struct {
+	TotalRecharge int64 `json:"totalRecharge" bson:"totalRecharge"` //总充值(单位元)
+	ScoreFlow     int64 `json:"scoreFlow" bson:"scoreFlow"`         //总流水
+	ByFlow        int64 `json:"byFlow" bson:"byFlow"`               //捕鱼流水
+	LhjFlow       int64 `json:"lhjFlow" bson:"lhjFlow"`             //老虎机流水
+}
 
 func main() {
-	cmd.Execute()
+	mk := PlayerRealFlow{
+		TotalRecharge: 10000,
+		ScoreFlow:     123,
+		ByFlow:        1234123412,
+		LhjFlow:       1234123412314,
+	}
+	str, _ := json.Marshal(mk)
+
+	dmk := string(str)
+	fmt.Println(dmk)
+	data := &PlayerRealFlow{}
+	if err := json.Unmarshal([]byte(dmk), data); err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("%+v", data)
+	if err := json.Unmarshal([]byte("{\"totalRecharge\":100000000,\"scoreFlow\":120000000,\"byFlow\":1012000000,\"lhjFlow\":1560000000}"), data); err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("%+v", data)
+
 }
