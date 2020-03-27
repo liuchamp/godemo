a830dd42c9579b58649d7d5f5e72496a3f7c5b39
yexingyun <yexingyun9210@gmail.com>
2020-03-27 12:04:28 +0700

    测试数据构架

--- main.go
+++ main.go
@@ -2,34 +2,35 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"io/ioutil"
 )
 
 func main() {
-	src, err := ioutil.ReadFile("pikm/test.go")
-	if err != nil {
-		panic(err)
-	}
+	//src, err := ioutil.ReadFile("pikm/test.go")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fset := token.NewFileSet()
+	//file, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//ast.Inspect(file, func(x ast.Node) bool {
+	//	s, ok := x.(*ast.StructType)
+	//	if !ok {
+	//		return true
+	//	}
+	//
+	//	for _, field := range s.Fields.List {
+	//		fmt.Printf("Field: %s\n", field.Names[0].Name)
+	//		fmt.Printf("Tag:   %s\n", field.Tag.Value)
+	//	}
+	//	return false
+	//})
+	//
 
-	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, "", src, parser.ParseComments)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println(len("5e5e107fb351a231750701f4"))
+	fmt.Println(len("ff4ecde7-826d-4660-985b-2b1354297971") * 100000)
 
-	ast.Inspect(file, func(x ast.Node) bool {
-		s, ok := x.(*ast.StructType)
-		if !ok {
-			return true
-		}
-
-		for _, field := range s.Fields.List {
-			fmt.Printf("Field: %s\n", field.Names[0].Name)
-			fmt.Printf("Tag:   %s\n", field.Tag.Value)
-		}
-		return false
-	})
 }
