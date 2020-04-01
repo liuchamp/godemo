49601749b4581a0c39fbbc7fe68c09b4079eb997
yexingyun <yexingyun9210@gmail.com>
2020-04-01 17:26:13 +0700

    测试ast的其他特性

--- pikm/test.go
+++ pikm/test.go
@@ -3,13 +3,14 @@ package pikm
 //db_start 刚开始
 //发现多个注解
 type AgentGameItem struct { //ssssss
-	RoundNum     uint32  `json:"roundNum" bson:"roundNum"`         //局数
-	WinScore     int64   `json:"winScore" bson:"winScore"`         //游戏消耗分数(输赢情况)
-	Tax          int64   `json:"tax" bson:"tax"`                   //税收
-	Flow         int64   `json:"flow" bson:"flow"`                 //流水
-	InvisibleTax int64   `json:"InvisibleTax" bson:"InvisibleTax"` //暗税
-	OnlinePlayer int64   `json:"onlinePlayer" bson:"onlinePlayer"` // 在线人数
-	Percentage   float32 `json:"percentage" bson:"percentage"`     //占百分比（game是type时，表示各个游戏间的百分比，game时kind时，各个场次的百分比）
+	RoundNum       uint32  `json:"roundNum" bson:"roundNum"`         //局数
+	WinScore       int64   `json:"winScore" bson:"winScore"`         //游戏消耗分数(输赢情况)
+	Tax            int64   `json:"tax" bson:"tax"`                   //税收
+	Flow           int64   `json:"flow" bson:"flow"`                 //流水
+	InvisibleTax   int64   `json:"InvisibleTax" bson:"InvisibleTax"` //暗税
+	OnlinePlayer   int64   `json:"onlinePlayer" bson:"onlinePlayer"` // 在线人数
+	Percentage     float32 `json:"percentage" bson:"percentage"`     //占百分比（game是type时，表示各个游戏间的百分比，game时kind时，各个场次的百分比）
+	AgentGamesItem "inline"
 }
 
 //db_start 刚开始
